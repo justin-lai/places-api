327b7adc4d6291030987d95e7e59786f3956f6d7
Justin Lai <justin.th.lai@gmail.com>
2016-08-14 22:44:20 -0700

    added linting and style guide

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
+import bindAll from 'lodash.bindall';
 import SearchFields from './SearchFields.jsx';
 import PlacesList from './PlacesList.jsx';
 import PlacesDetails from './PlacesDetails.jsx';
@@ -13,79 +14,41 @@ class App extends React.Component {
       location: new google.maps.LatLng(37.773972, -122.431297), // San Francisco
       places: [],
       currentPlace: null,
-      show: 'list'
-    }; 
+      show: 'list',
+    };
     this.markers = [];
     this.infoWindows = [];
     this.geocoder = new google.maps.Geocoder();
+    bindAll(this,
+      'getDetails',
+      'findAddress',
+      'handleClickEntry',
+      'handleQuerySubmit',
+      'returnToList'
+    );
   }
 
   componentDidMount() {
     this.map = new google.maps.Map(document.getElementById('gmap_canvas'), {
       center: this.state.location,
-      zoom: 15
+      zoom: 15,
     });
   }
 
-  handleQuerySubmit(params) {
-    this.setState({
-      keyword: params.keyword,
-      address: params.address,
-    }, this.findAddress.bind(this));
-  }
+  getDetails() {
+    const request = {
+      placeId: this.state.currentPlace.place_id,
+    };
 
-  handleClickEntry(place) {
-    this.markers.forEach(mark => {
-      if (mark.id === place.id) {
-        const infoWindow = new google.maps.InfoWindow({
-          content: `<img class="info-icon" src=${place.icon} />
-                    <div class="info-content-container">
-                      <p class="info-name"><strong>${place.name}</strong></p>
-                      <p class="info-address small">${place.formatted_address}</p>
-                    </div>`
-        });
-        this.clearInfoWindows();
-
-        this.infoWindows.push(infoWindow);
-        infoWindow.open(this.map, mark);
-      }
-    })
-    this.setState({
-      currentPlace: place,
-    }, this.getDetails.bind(this));
-  }
-
-  findAddress() {
-    // converts address into Lat and Lng coordinates
-    this.geocoder.geocode( { 'address': this.state.address}, (results, status) => {
-      if (status == google.maps.GeocoderStatus.OK) {
-
-        // center map at new address
-        const addressCoords = results[0].geometry.location;
-        this.map.setCenter(addressCoords);
+    const service = new google.maps.places.PlacesService(this.map);
+    service.getDetails(request, (details, status) => {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
         this.setState({
-          location: addressCoords,
-          show: 'list'
+          currentPlace: details,
+          show: 'details',
         });
-        
-        if (this.state.keyword) {
-          const options = {
-            location: this.state.location,
-            query: this.state.keyword
-          };
-
-          this.clearMarkers();
-          this.searchPlaces(options);
-        } else {
-          this.setState({
-            show: 'noResults'
-          })
-        }
       } else {
-        console.log(`Geocode failed: ${status}`);
-        this.setState({
-          show: 'addressError'
-        })
+        console.log(`Details request failed: ${status}`);
       }
     });
   }
@@ -95,9 +58,9 @@ class App extends React.Component {
     const service = new google.maps.places.PlacesService(this.map);
 
     service.textSearch(options, (results, status) => {
-      if (status == google.maps.places.PlacesServiceStatus.OK) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
         this.setState({
-          places: results
+          places: results,
         });
 
         for (let i = 0; i < results.length; i++) {
@@ -106,14 +69,14 @@ class App extends React.Component {
       } else {
         console.log(`Text search failed: ${status}`);
         this.setState({
-          show: 'noResults'
-        })
+          show: 'noResults',
+        });
       }
     });
   }
 
   clearMarkers() {
-    for (let i in this.markers) {
+    for (let i = 0; i < this.markers.length; i++) {
       this.markers[i].setMap(null);
     }
     this.markers = [];
@@ -121,7 +84,7 @@ class App extends React.Component {
   }
 
   clearInfoWindows() {
-    for (let i in this.infoWindows) {
+    for (let i = 0; i < this.infoWindows.length; i++) {
       if (this.infoWindows[i].getMap()) {
         this.infoWindows[i].close();
       }
@@ -133,7 +96,7 @@ class App extends React.Component {
       position: place.geometry.location,
       map: this.map,
       title: place.name,
-      id: place.id
+      id: place.id,
     });
     this.markers.push(mark);
 
@@ -142,51 +105,97 @@ class App extends React.Component {
                 <div class="info-content-container">
                   <p class="info-name"><strong>${place.name}</strong></p>
                   <p class="info-address small">${place.formatted_address}</p>
-                </div>`
+                </div>`,
     });
 
-    let context = this;
+    const context = this;
     google.maps.event.addListener(mark, 'click', () => {
       this.clearInfoWindows();
       infoWindow.open(this.map, mark);
 
       context.setState({
         currentPlace: place,
-      }, context.getDetails.bind(context));
+      }, context.getDetails);
     });
     this.infoWindows.push(infoWindow);
   }
 
-  getDetails() {
-    const request = {
-      placeId: this.state.currentPlace.place_id
-    };
 
-    const service = new google.maps.places.PlacesService(this.map);
-    service.getDetails(request, (details, status) => {
-      if (status == google.maps.places.PlacesServiceStatus.OK) {
+  findAddress() {
+    // converts address into Lat and Lng coordinates
+    this.geocoder.geocode({ address: this.state.address }, (results, status) => {
+      if (status === google.maps.GeocoderStatus.OK) {
+
+        // center map at new address
+        const addressCoords = results[0].geometry.location;
+        this.map.setCenter(addressCoords);
         this.setState({
-          currentPlace: details,
-          show: 'details'
-        })
+          location: addressCoords,
+          show: 'list',
+        });
+
+        if (this.state.keyword) {
+          const options = {
+            location: this.state.location,
+            query: this.state.keyword,
+          };
+
+          this.clearMarkers();
+          this.searchPlaces(options);
+        } else {
+          this.setState({
+            show: 'noResults',
+          });
+        }
       } else {
-        alert(`Details request failed: ${status}`);
+        console.log(`Geocode failed: ${status}`);
+        this.setState({
+          show: 'addressError',
+        });
       }
     });
   }
 
-  returnToList() { this.setState({ show: 'list' }) }
+  handleQuerySubmit(params) {
+    this.setState({
+      keyword: params.keyword,
+      address: params.address,
+    }, this.findAddress);
+  }
 
-  render () {
+  handleClickEntry(place) {
+    this.markers.forEach(mark => {
+      if (mark.id === place.id) {
+        const infoWindow = new google.maps.InfoWindow({
+          content: `<img class="info-icon" src=${place.icon} />
+                    <div class="info-content-container">
+                      <p class="info-name"><strong>${place.name}</strong></p>
+                      <p class="info-address small">${place.formatted_address}</p>
+                    </div>`,
+        });
+        this.clearInfoWindows();
+
+        this.infoWindows.push(infoWindow);
+        infoWindow.open(this.map, mark);
+      }
+    });
+    this.setState({
+      currentPlace: place,
+    }, this.getDetails;
+  }
+
+  returnToList() { this.setState({ show: 'list' }); }
+
+  render() {
     let bottomContent;
     if (this.state.show === 'list') {
-      bottomContent = <PlacesList places={this.state.places} handleClickEntry={this.handleClickEntry.bind(this)} />;
+      bottomContent = <PlacesList places={this.state.places} handleClickEntry={this.handleClickEntry} />;
     } else if (this.state.show === 'details') {
-      bottomContent = <PlacesDetails place={this.state.currentPlace} returnToList={this.returnToList.bind(this)} />;
+      bottomContent = <PlacesDetails place={this.state.currentPlace} returnToList={this.returnToList} />;
     } else if (this.state.show === 'addressError') {
       bottomContent = <h4 className="error-msg">Could not locate specified address</h4>;
     } else if (this.state.show === 'noResults') {
-      bottomContent = <h4 className="error-msg">No search results found</h4>
+      bottomContent = <h4 className="error-msg">No search results found</h4>;
     }
 
     return (
@@ -195,14 +204,14 @@ class App extends React.Component {
           <div id="content-container" className="col-xs-4 col-md-4 no-padding">
             <div id="content-top">
               <h1 className="title">What's Around!</h1>
-              <SearchFields handleQuerySubmit={this.handleQuerySubmit.bind(this)} />
+              <SearchFields handleQuerySubmit={this.handleQuerySubmit} />
             </div>
             <div id="content-bottom">
               {bottomContent}
             </div>
           </div>
           <div id="map-container" className="col-xs-8 col-md-8 no-padding">
-            <div id="gmap_canvas"></div>
+            <div id="gmap_canvas" />
           </div>
         </div>
       </section>
@@ -211,4 +220,4 @@ class App extends React.Component {
   }
 }
 
-render(<App/>, document.getElementById('app'));
+render(<App />, document.getElementById('app'));
